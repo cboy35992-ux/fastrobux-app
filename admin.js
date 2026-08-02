7e6f8538085671e3ab7081b8408d75ca7b741eca
cboy35992-ux <cboy35992@gmail.com>
2026-08-02 22:34:22 +0800

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url><loc>https://fastrobux-app.onrender.com/</loc><priority>1.0</priority></url>
+<url><loc>https://fastrobux-app.onrender.com/auth.html</loc><priority>0.7</priority></url>
+<url><loc>https://fastrobux-app.onrender.com/faq.html</loc><priority>0.7</priority></url>
+<url><loc>https://fastrobux-app.onrender.com/privacy.html</loc><priority>0.4</priority></url>
+<url><loc>https://fastrobux-app.onrender.com/terms.html</loc><priority>0.4</priority></url>
+<url><loc>https://fastrobux-app.onrender.com/refund.html</loc><priority>0.4</priority></url>
+</urlset>
